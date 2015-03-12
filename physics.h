5058aa6b8456433bbcb1f9ab1e324084fe470bb3
Henry J Schmale <hschmale16@gmail.com>
2015-03-11 23:08:14 -0400

    Started Physics.h defs

--- physics.h
+++ physics.h
@@ -7,6 +7,22 @@
 #ifndef PHYSICS_H_INC
 #define PHYSICS_H_INC
 
+struct vec2d{
+    double m_xC;       //!< x componet (newtons)
+    double m_yC;       //!< y componet (newtons)
+};
 
+struct chargeSrc{
+    double m_xPos;     //!< x posisition (meters)
+    double m_yPos;     //!< y posisition (meters)
+    double m_charge;   //!< electric charge
+
+    //default ctor
+    chargeSrc()
+        :m_xPos(0), m_yPos(0), m_charge(0){}
+
+    chargeSrc(double x, double y, double c)
+        :m_xPos(0), m_yPos(0), m_charge(0){}
+}
 
 #endif // PHYSICS_H_INC
