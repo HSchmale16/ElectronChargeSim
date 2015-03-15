a3eb270820f789b9e4b834a2ab1827064bf0d3e3
Henry J Schmale <hschmale16@gmail.com>
2015-03-15 17:42:30 -0400

    Starting over
    * Memory Errors when working with the ball, so I made a new
      branch starting from where it was last working.
    * Bounds checking for vectors

--- graphics.cpp
+++ graphics.cpp
@@ -57,13 +57,15 @@ void drawVectors(){
     int i = 0;
     for(double x = XMIN; x <= XMAX; x += DXY_RES){
         for(double y = YMIN; y <= YMAX; y += DXY_RES){
-            double angle = atan2(vectors[i].m_yC, vectors[i].m_xC);
-            board.drawArrow(
-                 .2 * cos(angle) * SCALE_FACTOR + x * SCALE_FACTOR,
-                 .2 * sin(angle) * SCALE_FACTOR + y * SCALE_FACTOR,
-                 x * SCALE_FACTOR,
-                 y * SCALE_FACTOR);
-            i++;
+            if(i < VECCOUNT){
+                double angle = atan2(vectors[i].m_yC, vectors[i].m_xC);
+                board.drawArrow(
+                    .1 * cos(angle) * SCALE_FACTOR + x * SCALE_FACTOR,
+                    .1 * sin(angle) * SCALE_FACTOR + y * SCALE_FACTOR,
+                    x * SCALE_FACTOR,
+                    y * SCALE_FACTOR);
+                i++;
+            }
         }
     }
 }
